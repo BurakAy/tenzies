87d64853363a6094019a94366234da90d8544aed
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-03 16:48:44 -0600

    gamestats component for displaying stats

---
+++ src/components/GameStats.jsx
@@ -0,0 +1,16 @@
+import "../styles/GameStats.css";
+
+const GameStats = (props) => {
+  return (
+    <div className="gamestats--container">
+      <div className="gamestats--wrapper">
+        <p className="gamestats-best_game">
+          Best Game: {props.lowestRolls ? props.lowestRolls : 0}
+        </p>
+        <p className="gamestats--rolls">Rolls: {props.diceRolls}</p>
+      </div>
+    </div>
+  );
+};
+
+export default GameStats;
